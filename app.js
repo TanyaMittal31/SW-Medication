79787a52a9991491ea7bfa3882d2c88815c198bd
Tanya Agrawal <tanya.agrawal_cs19@gla.ac.in>
2021-11-28 16:36:45 +0530

    adminlogin and login added

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 require(__dirname + "/register/db/connect");
 const Register = require(__dirname + "/register/models/reg");
+const Admin = require(__dirname + "/register/models/admin");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -15,6 +16,12 @@ app.use(express.static(static_path));
 
 app.get("/register" , (req, res) => {
     res.sendFile(__dirname + "/register/public/views/register.html");
+});
+app.get("/login" , (req, res) => {
+    res.sendFile(__dirname + "/register/public/views/login.html");
+});
+app.get("/adminlogin" , (req, res) => {
+    res.sendFile(__dirname + "/register/public/views/adminlogin.html");
 });
 
 // create a new user in database
@@ -35,6 +42,41 @@ app.post("/register" , async(req,res) => {
     }
     catch(e){
         res.status(400).send(e);
+    }
+})
+
+// login validation
+
+app.post("/login" , async(req,res) => {
+    try {
+        const email = req.body.email;
+        const password = req.body.pswd;
+        const useremail = await Register.findOne({email:email});
+        if(useremail.pswd === password){
+            res.status(201).sendFile(__dirname + "/register/public/views/success.html");
+        }
+        else{
+            res.send("invalid login details");
+        }
+    } catch (error) {
+        console.log("invalid login details");
+    }
+})
+
+// admin login validation
+app.post("/adminlogin" , async(req,res) => {
+    try {
+        const uname = req.body.uname;
+        const password = req.body.pswd;
+        const username = await Admin.findOne({uname:uname});
+        if(username.pswd === password){
+            res.status(201).sendFile(__dirname + "/register/public/views/success.html");
+        }
+        else{
+            res.send("invalid login details");
+        }
+    } catch (error) {
+        console.log("invalid login details");
     }
 })
 
