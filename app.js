317bb1a048e6e335ad44f93f0c0eee17e01debbb
Tanya Agrawal <tanya.agrawal_cs19@gla.ac.in>
2021-11-29 10:10:26 +0530

    donation page

--- app.js
+++ app.js
@@ -4,15 +4,20 @@ const hbs = require("hbs");
 const app = express();
 const port = process.env.PORT || 3000;
 require(__dirname + "/register/db/connect");
+require(__dirname + "/donation/db/connect");
 const Register = require(__dirname + "/register/models/reg");
 const Admin = require(__dirname + "/register/models/admin");
+const Donate = require(__dirname + "/donation/models/donate");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const static_path = path.join(__dirname + "/register/public");
 app.use(express.static(static_path));
-// app.set("view engine" , "hbs");
-// hbs.registerPartials(__dirname + "/register/templates/partials");
+const static_path1 = path.join(__dirname + "/donation/public");
+app.use(express.static(static_path1));
+const static_path2 = path.join(__dirname + "/donation/public/views");
+app.set("view engines" , "hbs");
+app.set("views" , path.join(static_path2))
 
 app.get("/register" , (req, res) => {
     res.sendFile(__dirname + "/register/public/views/register.html");
@@ -22,6 +27,28 @@ app.get("/login" , (req, res) => {
 });
 app.get("/adminlogin" , (req, res) => {
     res.sendFile(__dirname + "/register/public/views/adminlogin.html");
+});
+app.get("/donate" , (req, res) => {
+    res.render(__dirname + "/donation/public/views/donate.hbs");
+});
+app.get("/mydonations" , async(req, res) => {
+    try {
+        const email = req.body.cemail;
+        const phone = req.body.cphone;
+        const useremail = await Register.findOne({email:email});
+        registerInfo.find((err,docs) => {
+            if(useremail.phone === phone){
+                res.status(201).render("mydonations.hbs" , {
+                    list : docs
+                });
+            }
+            else{
+                res.send("Sorry No data Found");
+            }
+        });
+    } catch (error) {
+        res.status(400).send("invalid details");
+    }
 });
 
 // create a new user in database
@@ -79,6 +106,33 @@ app.post("/adminlogin" , async(req,res) => {
         console.log("invalid login details");
     }
 })
+
+// donate details
+app.post("/donate" , async(req,res) => {
+    try{
+        const donateDetails = new Donate({
+            fname : req.body.fname,
+            lname : req.body.lname,
+            cemail : req.body.cemail,
+            cphone : req.body.cphone,
+            caddress : req.body.caddress,
+            pname : req.body.pname,
+            mfg : req.body.mfg,
+            exp : req.body.exp,
+            price : req.body.price,
+            qty : req.body.qty,
+            pdesc : req.body.pdesc
+            
+        })
+        const registered = await donateDetails.save();
+        res.status(201).send("donation successful");
+    }
+    catch(e){
+        res.status(400).send(e);
+    }
+})
+
+// my donations page
 
 app.listen(port , () => {
     console.log(`server is running at port ${port}`);
